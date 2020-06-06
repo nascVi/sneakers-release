403f6484c40e367ee1c53ec72ed0246b4cfaeb2f
nascvi <vaiiho.nascimbene@gmail.com>
2020-06-06 09:58:06 +0200

    unessessary links and stack atm 🦮

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
-import ApolloClient from 'apollo-client' 
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { createHttpLink } from 'apollo-link-http'
-import { ApolloProvider } from '@apollo/react-hooks'
+// import ApolloClient from 'apollo-client' 
+// import { InMemoryCache } from 'apollo-cache-inmemory'
+// import { createHttpLink } from 'apollo-link-http'
+// import { ApolloProvider } from '@apollo/react-hooks'
 
 import Home from './components/home/Home'
 import Shoesone from './components/Shoesone'
@@ -18,18 +18,18 @@ import Footer from './components/header-footer/Footer'
 import Header from './components/header-footer/Header'
 
 
-const httpLink = new createHttpLink({
-    uri: "https://localhost:5000"
-  });
+// const httpLink = new createHttpLink({
+//     uri: "https://localhost:5000"
+//   });
 
-const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
-});
+// const client = new ApolloClient({
+//     link: httpLink,
+//     cache: new InMemoryCache()
+// });
 
 const Routes = () => (
     <BrowserRouter>
-        <ApolloProvider client={client}>
+        {/* <ApolloProvider client={client}> */}
             <Route path="/" render={props => (props.location.pathname !== '/') && <Header/> }/>
             <Route exact path="/" component={Home} />
             <Route exact path="/shoesone" component={Shoesone} />
@@ -37,7 +37,7 @@ const Routes = () => (
             <Route exact path="/custom" component={Custom} />
             <Route exact path="/cleaning" component={Cleaning} />
             <Footer />
-        </ApolloProvider>
+        {/* </ApolloProvider> */}
     </BrowserRouter>
 )
 
